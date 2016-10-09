320c3b2eb1c05ff9c27f5d2d624750e110afb2f7
Михаил Рычагов <mixrich1@tpu.ru>
2016-10-09 13:14:47 +0700

    Update 2016-10-09T06:14:39.746Z

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+// Categories
 var categoriesShowAllButton = document.getElementById('js-categories-show-all');
 
 if (categoriesShowAllButton) {
@@ -11,5 +11,77 @@ if (categoriesShowAllButton) {
                 toogles[i].classList.remove('hidden');
             }
         }
+    });
+}
+
+
+
+// Slider
+
+var slickConfig = {
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    arrows: true,
+    variableWidth: true,
+    prevArrow: '<button type="button" class="slick-prev">&#8592</button>',
+    nextArrow: '<button type="button" class="slick-next">&#8594;</button>',
+    responsive: [
+    {
+      breakpoint: 1160,
+      settings: {
+        arrows: false,
+        centerMode: false,
+        centerPadding: '40px',
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        variableWidth: false,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 959,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        adaptiveHeight: true,
+        variableWidth: true,
+        dots: false
+      }
+    },
+    {
+        breakpoint: 840,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          adaptiveHeight: true,
+          variableWidth: false,
+          dots: true
+        }
+    }
+    ]
+};
+
+//Берем все слайдеры на странице
+var sliders = $('.slider');
+
+//Если есть хотя бы один, то для каждого берем его картинки и оборачиваем в div, после чего инифиализируем
+if (sliders.length) {
+    sliders.each(function(index, slider){
+
+        var images = $(slider).find('img');
+
+        if (images.length) {
+            $(images).each(function(i, image){
+                $(image).wrap('<div class="slider__image-container"></div>');
+            });
+        }
+
+        $(slider).slick(slickConfig);
+
     });
 }

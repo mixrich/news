2b38c2bbec27b9283b3f8bcdb08f2e915668b572
Михаил Рычагов <mixrich1@tpu.ru>
2016-10-10 19:06:41 +0700

    add autoprefixes

--- gulpTasks/compileCSS.js
+++ gulpTasks/compileCSS.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
 
 function compile(options) {
 	return gulp.src("./src/css/_index.css")
 		.pipe(cleanCSS())
+		.pipe(autoprefixer({
+            browsers: ['last 16 versions']
+        }))
 		.pipe(rename('style.min.css'))
 		.pipe(gulp.dest("./dist/css"));
 }
